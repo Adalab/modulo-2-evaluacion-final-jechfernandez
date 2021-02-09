4cfe8b6859ee5d8d67914de4345a9a794f2f76f7
jechfernandez <jechfernandez@gmail.com>
2021-02-09 06:47:18 +0100

    Retrieve elements and fetch data from api in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log('>> Ready :)');
+//RETRIEVE ELEMENTS FROM HTML
+const apiLink = 'http://api.tvmaze.com/search/shows?q=';
+const formElement = document.querySelector ('.js-form');
+const searchInputElement = document.querySelector ('.js-search-input');
+const searchButtonElement = document.querySelector ('.js-search-button');
+const listElement = document.querySelector ('.js-list');
+
+//SERIES'S ARRAY
+let series = [];
+
+//PREVENT SUBMIT FORM
+document.querySelector(".js-search-button").addEventListener("click",
+    function (event) {
+    event.preventDefault()
+});
+
+//FETCH DATA FROM API
+
+function getApisData () {
+  const searchValue = searchInputElement.value;
+  fetch (apiLink + searchValue)
+    .then (response => response.json ())
+    .then (data => {
+      series = data;
+      paintSeriesCards ();
+    });
+}
+
+searchButtonElement.addEventListener ('click', getApisData);
+
+function paintSeriesCards() {
+
+}
